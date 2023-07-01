41a51294bb912cd0a6e6c42e018fa79a1a5ab584
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2023-07-01 01:31:30 -0700

    added sol constructor for transfer toeken

--- transfertokendapp/contracts/Contract.sol
+++ transfertokendapp/contracts/Contract.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+interface IERC20 {
+   function balanceOf(address account) external view returns (uint256);
+   function approve(address spender, uint256 amount) external returns (bool);
+   function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
 contract MyContract {
     constructor() {}
 }
