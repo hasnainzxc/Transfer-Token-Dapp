036a51fc35af2101b5a6319c5f98d70cadd64c87
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2023-07-02 10:39:59 -0700

    Update hardhat.config.js

--- transfertokendapp/hardhat.config.js
+++ transfertokendapp/hardhat.config.js
@@ -25,19 +25,4 @@ module.exports = {
       zksync: true,
     },
   },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-};
+ 
