c51016db7903fdb23cb8e745e63f03d2cb73974d
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2023-07-04 08:07:46 -0700

    components for UI

--- ttdapp/pages/_app.tsx
+++ ttdapp/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
-
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/NavBar";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
@@ -10,9 +10,12 @@ const activeChain = "ethereum";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
+      <ChakraProvider>
       <Component {...pageProps} />
+      <Navbar></Navbar>
+      </ChakraProvider>
     </ThirdwebProvider>
   );
-}
+}   
 
 export default MyApp;
