4bebb7c928834bda66359efce3afc206d6ca219d
Hasnian <52049324+Hasnainzxc@users.noreply.github.com>
2023-07-06 11:23:44 -0700

    ui componenets for token transfer added

--- ttdapp/pages/transfer.tsx
+++ ttdapp/pages/transfer.tsx
@@ -1,14 +1,32 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Container, Flex, Box, useBreakpointValue } from "@chakra-ui/react";
 import TransferCard from "../components/TransferCard";
 import Events from "../components/Events";
 
 export default function TransferPage() {
-    return (
-        <Container maxW={"1440px"}>
-            <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
-                <TransferCard />
-                <Events />
-            </Flex>
-        </Container>
-    );
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  return (
+    <Container maxW="1440px">
+      <Flex
+        direction={isMobile ? "column" : "row"}
+        justifyContent="center"
+        alignItems="center"
+        h="100vh"
+      >
+        <Box flex="1">
+          <TransferCard />
+        </Box>
+        {!isMobile && (
+          <Box flex="1" ml={8}>
+            <Events />
+          </Box>
+        )}
+      </Flex>
+      {isMobile && (
+        <Box mt={4}>
+          <Events />
+        </Box>
+      )}
+    </Container>
+  );
 }
